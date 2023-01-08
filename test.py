d87a79e486ce790dfc6958f1cd524e44fe6461d9
maxoganian <62914182+maxoganian@users.noreply.github.com>
2023-01-08 13:45:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ pygame.init()
 
 running = True
 
+sound = pygame.mixer.Sound("sounds/im_back.wav")
+
+sound.set_volume(.2)
+
 while running:
 
-	pygame.mixer.Sound.play(pygame.mixer.Sound("sounds/im_back.wav").set_volume(0.5))
+	pygame.mixer.Sound.play(sound)
